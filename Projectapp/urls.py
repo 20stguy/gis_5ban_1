0b58f362bc7c3c072bef70e819acb4a9b7e7676d
20stguy <20stguy+git@gmail.com>
2021-08-17 16:28:10 +0900

    8/1 5ban class commit 1

--- Projectapp/urls.py
+++ Projectapp/urls.py
@@ -1,6 +1,12 @@
+from django.urls import path
 
+from Projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListView
 
+app_name = 'projectapp'
 
 urlpatterns = [
+    path('create/', ProjectCreateView.as_view(), name='create'),
+    path('detail/<ink:pk>', ProjectDetailView.as_view(), name='detail'),
 
+    path('list/', ProjectListView.as_view(), name='list'),
 ]
